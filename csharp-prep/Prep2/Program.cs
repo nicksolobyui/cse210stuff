58a1faa516cfba43e5e137abf42b65cc7bd2f88c
Nikita <nicksolov8203@gmail.com>
2024-10-01 18:30:40 -0600

    task done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string userGrade = Console.ReadLine();
+
+        int number = int.Parse(userGrade);
+
+        if (number >= 90)
+        {
+            Console.WriteLine("The grade is A");
+        }
+
+        else if (number >= 80)
+        {
+            Console.WriteLine("The grade is B");
+        }
+
+        else if (number >= 70)
+        {
+            Console.WriteLine("The grade is C");
+        }
+
+        else if (number >= 60)
+        {
+            Console.WriteLine("The grade is D");
+        }
+
+        else
+        {
+            Console.WriteLine("The grade is F");
+        }
+
     }
 }
